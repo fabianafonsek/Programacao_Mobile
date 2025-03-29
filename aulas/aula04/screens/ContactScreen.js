a165939358143b42e1466915d0168274aaa8b062
Fabiana Fonseca <fabianafonsek@gmail.com>
2025-03-28 21:46:32 -0300

    Adicionando arquivos da aula04

---
+++ aulas/aula04/screens/ContactScreen.js
@@ -0,0 +1,37 @@
+import { View, FlatList, StyleSheet } from "react-native";
+import { Appbar, FAB, List } from "react-native-paper";
+
+const contatos = [
+  { id: 1, nome: "Mãe", fone: "61 99999-9999" },
+  { id: 2, nome: "Pai", fone: "61 98888-8888" },
+  { id: 3, nome: "Mano", fone: "61 9777-7777" },
+];
+
+function ContactScreen() {
+  return (
+    <View>
+      <Appbar.Header>
+        <Appbar.Content title="Contatos" />
+        <Appbar.Action icon="dots-vertical" />
+      </Appbar.Header>
+      <FlatList
+        data={contatos}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <List.Item
+            left={(props) => <List.Icon {...props} icon="account" />}
+            title={item.nome}
+            description={item.fone}
+          />
+        )}
+      />
+      <View>
+        <FAB icon="plus" size="small" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default ContactScreen;
