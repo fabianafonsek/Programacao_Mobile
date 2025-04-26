3b04f59dec9c77d584540b12f80d7703b3ffe08d
Fabiana Fonseca <fabianafonsek@gmail.com>
2025-04-25 21:55:28 -0300

    Adicionando arquivos da aula06

---
+++ aulas/aula06/App.js
@@ -0,0 +1,17 @@
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import NavegacaoPilha from "./routes/NavegacaoPilha";
+import NavegacaoAbaInferior from "./routes/NavegacaoAbaInferior";
+import NavegacaoGaveta from "./routes/NavegacaoGaveta";
+
+function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <NavegacaoGaveta/>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
+
+export default App; 
